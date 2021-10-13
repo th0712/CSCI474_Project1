bb4859d8893461d81a2b972d1a8edd0ec8b10aa3
th0712 <tomhoskins0712@gmail.com>
2021-10-13 18:51:40 -0500

    Formatting tweaks

--- main.c
+++ main.c
@@ -10,13 +10,16 @@
 #define LARGE 10000000000
 
 /* Function Declarations */
-float oneProcess(int size);
-float twoProcesses(int size);
+float OneProcess(int size);
+float TwoProcesses(int size);
+float FourProcesses(int size);
+
+/* Main Function */
 int main()
 {
    time_t small1StartTime = time(NULL);
    clock_t small1Begin = clock();
-   float sum1 = oneProcess(SMALL);
+   float sum1 = OneProcess(SMALL);
    clock_t small1End = clock();
    time_t small1EndTime = time(NULL);
    double small1ExecutionTime = (double)(small1End - small1Begin) / CLOCKS_PER_SEC;    //Execution time
@@ -26,7 +29,7 @@ int main()
 
    time_t small2StartTime = time(NULL);
    clock_t small2Begin = clock();
-   float sum2 = twoProcesses(SMALL);
+   float sum2 = TwoProcesses(SMALL);
    clock_t small2End = clock();
    time_t small2EndTime = time(NULL);
    double small2ExecutionTime = (double)(small2End - small2Begin) / CLOCKS_PER_SEC;
@@ -37,7 +40,7 @@ int main()
 return 0;
 }
 
-float oneProcess(int size)
+float OneProcess(int size)
 {
    float sum = 0;
    float tempVal;
@@ -48,7 +51,7 @@ float oneProcess(int size)
    }
    return sum;
 }
-float twoProcesses(int size)
+float TwoProcesses(int size)
 {
    float sum1 = 0;
    float sum2 = 0;
@@ -80,4 +83,37 @@ float twoProcesses(int size)
     sum1 += tempSum;
    }
 return sum1;
+}
+float FourProcesses(int size)
+{
+    float sum1 = 0;
+    float sum2 = 0;
+    float tempVal1, tempVal2, tempSum;
+    int pipe1[2];
+    if (pipe(pipe1) < 0)
+        exit(1);
+    int p = fork();
+    if (p == 0) //child
+    {
+        close(pipe1[0]);
+        for (int i = 0; i < (size / 2); i++)
+        {
+            tempVal2 = (float)rand() / (float)RAND_MAX;
+            sum2 += tanh(tempVal2);
+        }
+        write(pipe1[1], &sum2, sizeof(sum2));
+        exit(0);
+    }
+    else //parent
+    {
+        close(pipe1[1]);
+        for (int i = 0; i < (size / 2); i++)
+        {
+            tempVal1 = (float)rand() / (float)RAND_MAX;
+            sum1 = tanh(tempVal1);
+        }
+        read(pipe1[0], &tempSum, sizeof(tempSum));
+        sum1 += tempSum;
+    }
+    return sum1;
 }
